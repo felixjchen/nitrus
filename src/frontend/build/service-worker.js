d40cb1f1a067866b51b7cd136489e5d084c0d9da
Felix Chen <felixchen1998@gmail.com>
2020-10-05 00:15:56 -0400

    build

--- src/frontend/build/service-worker.js
+++ src/frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.986ce233998d2fa9ced33f3eef142c6b.js"
+  "./precache-manifest.3f0384000c01bdc64f2dbe941f57cf97.js"
 );
 
 workbox.clientsClaim();
--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -4,21 +4,12 @@ import io from "socket.io-client";
 import Page from "./lib/page";
 
 const urlSearchParams = new URLSearchParams(window.location.search);
-let urlParams = {};
-for (let entry of urlSearchParams) {
-  const [key, value] = entry;
-  urlParams[key] = value;
-}
-console.log(urlParams);
-
+const id = urlSearchParams.get("id");
 // https://www.npmjs.com/package/socket.io-client
 const backendAddress = "http://0.0.0.0";
 const socket = io(backendAddress);
 socket.on("connect", () => {
-  console.log("Socket connected to server.");
+  console.log(`Used with id ${id}, socket connected to server.`);
 });
 
-render(
-  <Page profileUrl={urlParams.profileUrl} />,
-  document.getElementById("root")
-);
+render(<Page />, document.getElementById("root"));
