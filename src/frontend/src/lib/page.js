bc278bb41937c10503c2754f7d9840bd1e001f76
Felix Chen <felixchen1998@gmail.com>
2020-10-07 22:41:03 -0400

    search result

--- src/frontend/src/lib/page.js
+++ src/frontend/src/lib/page.js
@@ -4,25 +4,17 @@ import {
   Header,
   HeaderName,
   HeaderNavigation,
-  HeaderMenu,
-  HeaderMenuItem,
-  HeaderMenuButton,
   HeaderContainer,
   HeaderGlobalBar,
   HeaderGlobalAction,
-  SideNav,
-  SideNavMenuItem,
-  SideNavItems,
-  SideNavLink,
-  SideNavMenu,
   Search,
   Grid,
   Row,
   Column,
 } from "carbon-components-react";
 import SwipeableBottomSheet from "react-swipeable-bottom-sheet";
-import { Fade16 } from "@carbon/icons-react";
 import { Logout20 } from "@carbon/icons-react";
+import { SearchResult } from "./search-result";
 import styles from "./page.css";
 
 const bottomSheetProps = {
@@ -34,23 +26,23 @@ const bottomSheetProps = {
 };
 
 class Page extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
       <div id="page">
         <HeaderContainer
           render={({ isSideNavExpanded, onClickSideNavExpand }) => (
             <>
-              <Header aria-label="Nectar">
+              <Header aria-label="Nitrous">
                 <HeaderName
                   // href="https://github.com/felixjchen/spotify-together"
                   prefix=""
                 >
-                  Nectar
+                  Nitrous
                 </HeaderName>
-                <HeaderNavigation aria-label="Nectar">
+                <HeaderNavigation aria-label="Nitrous">
                   <img src={this.props.profileUrl}></img>
                 </HeaderNavigation>
                 <HeaderGlobalBar>
@@ -71,11 +63,17 @@ class Page extends React.Component {
                     Queue
                   </Column>
                   <Column sm={4} md={6} lg={9}>
+                    {/* How do we pass input to sibling element in React??? */}
                     <Search
                       id="search"
                       placeHolderText={"Find a song"}
                       size={"lg"}
+                      labelText="search"
+                      onChange={() => {
+                        console.log(document.getElementById("search").value);
+                      }}
                     />
+                    <SearchResult search={"d"}></SearchResult>
                   </Column>
 
                   <Column sm={1} md={{ span: 0 }} lg={{ span: 0 }}>
@@ -83,7 +81,7 @@ class Page extends React.Component {
                       <div
                         id="queue"
                         style={{
-                          "background-color": "#f4f4f4;",
+                          backgroundColor: "#f4f4f4",
                           height: "calc(100vh - 3rem)",
                         }}
                       >
---
+++ src/frontend/src/lib/search-result.js
@@ -0,0 +1,128 @@
+import React from "react";
+// import { render } from "react-dom";
+import {
+  DataTable,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableHeader,
+  TableBody,
+  TableCell,
+} from "carbon-components-react";
+import styles from "./search-result.css";
+const searchResultProps = {
+  rows: [
+    {
+      id: "a",
+      name: "Load Balancer 3",
+      protocol: "HTTP",
+      port: 3000,
+      rule: "Round robin",
+      attached_groups: "Kevin’s VM Groups",
+      status: "Disabled",
+      enabled: true,
+    },
+    {
+      id: "b",
+      name: "Load Balancer 1",
+      protocol: "HTTP",
+      port: 443,
+      rule: "Round robin",
+      attached_groups: "Maureen’s VM Groups",
+      status: "Starting",
+      enabled: true,
+    },
+    {
+      id: "c",
+      name: "Load Balancer 2",
+      protocol: "HTTP",
+      port: 80,
+      rule: "DNS delegation",
+      attached_groups: "Andrew’s VM Groups",
+      status: "Active",
+      enabled: false,
+    },
+  ],
+  headers: [
+    {
+      key: "name",
+      header: "Name",
+    },
+    {
+      key: "protocol",
+      header: "Protocol",
+    },
+    {
+      key: "port",
+      header: "Port",
+    },
+    {
+      key: "rule",
+      header: "Rule",
+    },
+    {
+      key: "attached_groups",
+      header: "Attached Groups",
+    },
+    {
+      key: "status",
+      header: "Status",
+    },
+    {
+      key: "enabled",
+      header: "Enabled",
+    },
+  ],
+};
+class SearchResult extends React.Component {
+  // We need a function to get from search API
+  //
+  render() {
+    return (
+      <DataTable {...searchResultProps}>
+        {({
+          rows,
+          headers,
+          getHeaderProps,
+          getRowProps,
+          getTableProps,
+          getTableContainerProps,
+        }) => (
+          <TableContainer
+            title={this.props.search}
+            description="Usage example"
+            {...getTableContainerProps()}
+          >
+            <Table {...getTableProps()} isSortable>
+              <TableHead>
+                <TableRow>
+                  {headers.map((header) => (
+                    <TableHeader
+                      key={header.key}
+                      {...getHeaderProps({ header })}
+                      isSortable
+                    >
+                      {header.header}
+                    </TableHeader>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows.map((row) => (
+                  <TableRow key={row.id} {...getRowProps({ row })}>
+                    {row.cells.map((cell) => (
+                      <TableCell key={cell.id}>{cell.value}</TableCell>
+                    ))}
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        )}
+      </DataTable>
+    );
+  }
+}
+
+export { SearchResult };
