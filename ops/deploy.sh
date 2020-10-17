5bbfd36d826edbe7128416d2e677e97bfae1e2db
Felix Chen <felixchen1998@gmail.com>
2020-10-16 21:47:45 -0400

    deploy to azure

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -1,4 +1,2 @@
-docker build ../src/backend/. -t registry.heroku.com/nitrouz/web
-docker push registry.heroku.com/nitrouz/web
-
-heroku container:release web --app=nitrouz
+docker build ../src/backend/. -t felixchen1998/nitrous:latest
+docker push felixchen1998/nitrous:latest
