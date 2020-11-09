ccfb53680d9008db2d395e9bdc39567b4a5101da
Felix Chen <felixchen1998@gmail.com>
2020-11-08 21:36:09 -0500

    Update spotify-player.ts

--- dev/backend/lib/spotify-player.ts
+++ dev/backend/lib/spotify-player.ts
@@ -5,38 +5,45 @@ import * as fetch from "node-fetch";
 // for user with access_token, play track with URI, starting at position_ms
 const playerStart = (access_token, URI, position_ms) => {
   //https://developer.spotify.com/documentation/web-api/reference/player/start-a-users-playback/
-  var myHeaders = {"Authorization": "Bearer " + access_token, "Content-Type": "application/json"};
+  var myHeaders = {
+    Authorization: "Bearer " + access_token,
+    "Content-Type": "application/json",
+  };
 
-  var raw = JSON.stringify({"uris": [URI],"offset":{"position":0},"position_ms":position_ms});
+  var raw = JSON.stringify({
+    uris: [URI],
+    offset: { position: 0 },
+    position_ms: position_ms,
+  });
 
   var requestOptions = {
-    method: 'PUT',
+    method: "PUT",
     headers: myHeaders,
     body: raw,
-    redirect: 'follow'
+    redirect: "follow",
   };
-  
+
   fetch("https://api.spotify.com/v1/me/player/play", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
 };
 
 // for user with access_token, pause player
 const playerPause = (access_token) => {
   // https://developer.spotify.com/documentation/web-api/reference/player/pause-a-users-playback/
-  var myHeaders = {"Authorization": "Bearer " + access_token};
+  var myHeaders = { Authorization: "Bearer " + access_token };
 
   var requestOptions = {
-    method: 'PUT',
+    method: "PUT",
     headers: myHeaders,
-    redirect: 'follow'
+    redirect: "follow",
   };
 
   fetch("https://api.spotify.com/v1/me/player/pause", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
 };
 
 // for each user in room, play top track in room queue, starting at position_ms
@@ -44,9 +51,9 @@ const roomStart = (room, position_ms) => {
   //https://developer.spotify.com/documentation/web-api/reference/player/start-a-users-playback/
   var users = room.users;
   var track = room.queue[0];
-  for(var _ in users) {
+  for (var _ in users) {
     var access_token = users.user.accessToken;
-    playerStart(access_token, track, position_ms)
+    playerStart(access_token, track, position_ms);
   }
 };
 
